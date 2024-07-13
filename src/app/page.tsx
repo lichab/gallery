057beb698138de576507874f82a2cf2ff600bd6c
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2024-07-13 19:43:22 +0100

    make ui use db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-const mockImageUrls = [
-  "https://utfs.io/f/936bec9f-5fee-441c-9f67-aab85297db95-85pb9y.jpg",
-  "https://utfs.io/f/ba2a2d63-681f-461a-98a5-da07c89e4203-24ac7.jpg",
-  "https://utfs.io/f/d013c700-4d08-4feb-b6b7-5fd9ba415af9-imy4eq.jpg",
-  "https://utfs.io/f/dec15d92-d999-41f4-9491-1eaab44c0f87-8cfb8n.jpg",
-];
-
-const images = mockImageUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+import { db } from "~/server/db";
 
 export default async function HomePage() {
+  const images = await db.query.images.findMany();
+
   return (
     <main>
       <div className="flex flex-wrap items-center gap-4">
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,7 +18,7 @@ import {
  */
 export const createTable = pgTableCreator((name) => `gallery_${name}`);
 
-export const posts = createTable(
+export const images = createTable(
   "image",
   {
     id: serial("id").primaryKey(),
