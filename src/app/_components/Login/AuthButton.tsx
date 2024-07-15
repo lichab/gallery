35ca0ea2e2b92bef7d7ee47c21ca2347cd269edc
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2024-07-15 21:47:40 +0100

    add logout

---
+++ src/app/_components/Login/AuthButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { type User } from "lucia";
+import { useTransition } from "react";
+import type { ActionResult } from "~/app/layout";
+
+export default function AuthButton({
+  user,
+  action,
+}: {
+  user: User | null;
+  action: () => Promise<ActionResult>;
+}) {
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmit = () => {
+    startTransition(async () => {
+      await action();
+    });
+  };
+
+  if (isPending) {
+    return <div>Loading....</div>;
+  }
+  if (user) {
+    return (
+      <form action={handleSubmit}>
+        <button>Logout</button>
+      </form>
+    );
+  }
+
+  return (
+    <form action={handleSubmit}>
+      <button>Log in</button>
+    </form>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { validateRequest } from "~/server/validate-request";
+import AuthButton from "./_components/Login/AuthButton";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { lucia } from "~/auth";
 
 export const metadata = {
   title: "Gallery",
@@ -8,14 +13,42 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+async function logout(): Promise<ActionResult> {
+  "use server";
+  const { session } = await validateRequest();
+  if (!session) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+  return redirect("/");
+}
+
+async function login(): Promise<never> {
+  "use server";
+  return redirect("login/github");
+}
+
+export interface ActionResult {
+  error: string | null;
+}
+
 async function TopNav() {
+  const { user } = await validateRequest();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <h2>My Gallery</h2>
 
-      <p>
-        Sing In <a href="/login/github">Sign in with GitHub</a>
-      </p>
+      <AuthButton user={user} action={user ? logout : login} />
     </nav>
   );
 }
